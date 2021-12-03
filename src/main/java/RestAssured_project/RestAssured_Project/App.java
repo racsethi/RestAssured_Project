a1f5e1e416bb48b2fc23043f847e4ae6d9c36871
racsethi <racsethit@cisco.com>
2021-12-03 12:15:46 +0000

    RestAssures_Project

---
+++ src/main/java/RestAssured_project/RestAssured_Project/App.java
@@ -0,0 +1,13 @@
+package RestAssured_project.RestAssured_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
